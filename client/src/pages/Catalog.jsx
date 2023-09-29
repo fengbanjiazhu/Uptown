b0499c51a726eaf6a06ce5c500f5539814a46afc
Jeffrey <562475805lanu@gmail.com>
2023-09-29 22:00:29 +1000

    add filter with useSearchParams

--- client/src/pages/Catalog.jsx
+++ client/src/pages/Catalog.jsx
@@ -3,14 +3,10 @@ import React, { useCallback, useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 
 import Helmet from "../components/Helmet";
-import CheckBox from "../components/CheckBox";
-
-import category from "../assets/fake-data/category";
-import colors from "../assets/fake-data/product-color";
-import size from "../assets/fake-data/product-size";
-import Button from "../components/Button";
+import Filter from "../components/Filter";
 
 import InfinityList from "../components/InfinityList";
+import { useSearchParams } from "react-router-dom";
 
 const initFilter = {
   category: [],
@@ -22,39 +18,45 @@ const Catalog = () => {
   const productRedux = useSelector((state) => state.productModal.value);
   const [products, setProducts] = useState(productRedux);
   const [filter, setFilter] = useState(initFilter);
+  const [searchParams] = useSearchParams();
 
-  const filterSelect = (type, checked, item) => {
-    if (checked) {
-      switch (type) {
-        case "CATEGORY":
-          setFilter({ ...filter, category: [...filter.category, item.categorySlug] });
-          break;
-        case "COLOR":
-          setFilter({ ...filter, color: [...filter.color, item.color] });
-          break;
-        case "SIZE":
-          setFilter({ ...filter, size: [...filter.size, item.size] });
-          break;
-        default:
-      }
-    } else {
-      switch (type) {
-        case "CATEGORY":
-          const newCategory = filter.category.filter((e) => e !== item.categorySlug);
-          setFilter({ ...filter, category: newCategory });
-          break;
-        case "COLOR":
-          const newColor = filter.color.filter((e) => e !== item.color);
-          setFilter({ ...filter, color: newColor });
-          break;
-        case "SIZE":
-          const newSize = filter.size.filter((e) => e !== item.size);
-          setFilter({ ...filter, size: newSize });
-          break;
-        default:
-      }
-    }
-  };
+  const filterCategory = searchParams.get("category") || "all";
+  const filterColor = searchParams.get("color") || "all";
+  const filterSize = searchParams.get("size") || "all";
+
+  let filterCate;
+
+  if (filterCategory === "all") filterCate = productRedux;
+  if (filterCategory === "t-shirt")
+    filterCate = productRedux.filter((item) => item.categorySlug === "T-shirt");
+  if (filterCategory === "shirt")
+    filterCate = productRedux.filter((item) => item.categorySlug === "shirt");
+  if (filterCategory === "jeans")
+    filterCate = productRedux.filter((item) => item.categorySlug === "jean");
+
+  let filterCo;
+
+  if (filterColor === "all") filterCo = filterCate;
+  if (filterColor === "white")
+    filterCo = filterCate.filter((item) => item.colors.includes("white"));
+  if (filterColor === "pink") filterCo = filterCate.filter((item) => item.colors.includes("pink"));
+  if (filterColor === "red") filterCo = filterCate.filter((item) => item.colors.includes("red"));
+  if (filterColor === "yellow")
+    filterCo = filterCate.filter((item) => item.colors.includes("yellow"));
+  if (filterColor === "blue") filterCo = filterCate.filter((item) => item.colors.includes("blue"));
+  if (filterColor === "orange")
+    filterCo = filterCate.filter((item) => item.colors.includes("orange"));
+  if (filterColor === "black")
+    filterCo = filterCate.filter((item) => item.colors.includes("black"));
+
+  let filterS;
+
+  if (filterSize === "all") filterS = filterCo;
+  if (filterSize === "s") filterS = filterCo.filter((item) => item.size.includes("s"));
+  if (filterSize === "m") filterS = filterCo.filter((item) => item.size.includes("m"));
+  if (filterSize === "l") filterS = filterCo.filter((item) => item.size.includes("l"));
+  if (filterSize === "xl") filterS = filterCo.filter((item) => item.size.includes("xl"));
+  if (filterSize === "xxl") filterS = filterCo.filter((item) => item.size.includes("xxl"));
 
   const clearFilter = () => {
     setFilter(initFilter);
@@ -93,73 +95,47 @@ const Catalog = () => {
     updateProducts();
   }, [updateProducts]);
 
-  const filterRef = useRef(null);
-
-  const showHideFilter = () => filterRef.current.classList.toggle("active");
-
   return (
     <Helmet title="Uptown | Products">
       <div className="catalog">
-        <div className="catalog__filter" ref={filterRef}>
-          <div className="catalog__filter__close" onClick={() => showHideFilter()}>
-            <i className="bx bx-left-arrow-alt"></i>
-          </div>
-          <div className="catalog__filter__widget">
-            <div className="catalog__filter__widget__title">Product</div>
-            <div className="catalog__filter__widget__content">
-              {category.map((item, index) => (
-                <div key={index} className="catalog__filter__widget__content__item">
-                  <CheckBox
-                    label={item.display}
-                    onChange={(input) => filterSelect("CATEGORY", input.checked, item)}
-                    checked={filter.category.includes(item.categorySlug)}
-                  />
-                </div>
-              ))}
-            </div>
-          </div>
+        <div className="catalog__filter">
+          <Filter
+            filterField="category"
+            options={[
+              { value: "all", label: "All" },
+              { value: "t-shirt", label: "T-shirt" },
+              { value: "shirt", label: "Shirt" },
+              { value: "jeans", label: "Jeans" },
+            ]}
+          />
+          <Filter
+            filterField="color"
+            options={[
+              { value: "all", label: "All" },
+              { value: "white", label: "white" },
+              { value: "pink", label: "pink" },
+              { value: "black", label: "black" },
+              { value: "yellow", label: "yellow" },
+              { value: "orange", label: "orange" },
+              { value: "blue", label: "blue" },
+            ]}
+          />
 
-          <div className="catalog__filter__widget">
-            <div className="catalog__filter__widget__title">Color</div>
-            <div className="catalog__filter__widget__content">
-              {colors.map((item, index) => (
-                <div key={index} className="catalog__filter__widget__content__item">
-                  <CheckBox
-                    label={item.display}
-                    onChange={(input) => filterSelect("COLOR", input.checked, item)}
-                    checked={filter.color.includes(item.color)}
-                  />
-                </div>
-              ))}
-            </div>
-          </div>
-
-          <div className="catalog__filter__widget">
-            <div className="catalog__filter__widget__title">Size</div>
-            <div className="catalog__filter__widget__content">
-              {size.map((item, index) => (
-                <div key={index} className="catalog__filter__widget__content__item">
-                  <CheckBox
-                    label={item.display}
-                    onChange={(input) => filterSelect("SIZE", input.checked, item)}
-                    checked={filter.size.includes(item.size)}
-                  />
-                </div>
-              ))}
-            </div>
-          </div>
-
-          <div className="catalog__filter__widget">
-            <div className="catalog__filter__widget__content">
-              <Button size="sm" onClick={clearFilter}>
-                Clear filters
-              </Button>
-            </div>
-          </div>
+          <Filter
+            filterField="size"
+            options={[
+              { value: "all", label: "All" },
+              { value: "s", label: "S" },
+              { value: "m", label: "M" },
+              { value: "l", label: "L" },
+              { value: "xl", label: "XL" },
+              { value: "xxl", label: "XXL" },
+            ]}
+          />
         </div>
 
         <div className="catalog__content">
-          <InfinityList data={products} />
+          <InfinityList data={filterS} />
         </div>
       </div>
     </Helmet>
