3ddf0b0c52cb39d8e5216d9a0f7727346f374a1c
Jeffrey <562475805lanu@gmail.com>
2023-10-02 10:07:05 +1100

    temporary close chatbot api due to fee issue

--- server/Controller/chatbotController.js
+++ server/Controller/chatbotController.js
@@ -4,30 +4,37 @@ const appErr = require("../Utils/appError");
 const { v4: uuidv4 } = require("uuid");
 
 exports.createRes = catchAsync(async (req, res, next) => {
-  const privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, "\n");
-  const chatbot = new Chatbot(process.env.PROJECT_ID, privateKey, process.env.CLIENT_EMAIL);
+  // const privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, "\n");
+  // const chatbot = new Chatbot(process.env.PROJECT_ID, privateKey, process.env.CLIENT_EMAIL);
 
-  let text = req.body.text;
-  const uuid = uuidv4();
-  let context = "";
+  // let text = req.body.text;
+  // const uuid = uuidv4();
+  // let context = "";
 
-  let intentData = await chatbot.detectIntent(uuid, text, context, "en-US");
+  // let intentData = await chatbot.detectIntent(uuid, text, context, "en-US");
 
-  if (!intentData) return next(new appErr("Chatbot is having problem. Try again later", 404));
+  // if (!intentData) return next(new appErr("Chatbot is having problem. Try again later", 404));
 
-  const { fulfillmentMessages: customRespone } = intentData.queryResult;
+  // const { fulfillmentMessages: customRespone } = intentData.queryResult;
 
-  const responseData = customRespone[0];
+  // const responseData = customRespone[0];
 
-  if (responseData.payload) {
-    return res.status(200).json({
-      status: "success",
-      response: JSON.stringify(responseData.payload.fields),
-    });
-  }
+  // if (responseData.payload) {
+  //   return res.status(200).json({
+  //     status: "success",
+  //     response: JSON.stringify(responseData.payload.fields),
+  //   });
+  // }
+
+  // res.status(200).json({
+  //   status: "success",
+  //   text: responseData.text.text[0],
+  // });
+
+  const tempResponseData = "We currently closed chatbot API, please try again later";
 
   res.status(200).json({
     status: "success",
-    text: responseData.text.text[0],
+    text: tempResponseData,
   });
 });
