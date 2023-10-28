672787c93e0bef67da573d5fe7c9ca8362774df7
Jeffrey <562475805lanu@gmail.com>
2023-10-29 10:49:12 +1100

    removes unwanted import

--- client/src/pages/Booking.jsx
+++ client/src/pages/Booking.jsx
@@ -1,5 +1,5 @@
 import Helmet from "../components/Helmet";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { urlMeasuring, urlBooking } from "../api";
 
